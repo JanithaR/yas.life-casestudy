9a299e4511b2950ff5f1dc3022f8abf010413761
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2020-01-13 12:20:57 +0530

    Unsuccessful fetchLatestRatesGenerator() test

--- YasLifeCaseStudy/src/actions/converter.actions.test.ts
+++ YasLifeCaseStudy/src/actions/converter.actions.test.ts
@@ -65,5 +65,28 @@ describe('Converter actions', () => {
 		it('should be defined', () => {
 			expect(fetchLatestRatesGenerator).toBeDefined();
 		});
+
+		it('should dispatch an action', () => {
+			beforeAll(() => {
+				jest.mock('../api.ts');
+			});
+
+			expect(fetchLatestRatesGenerator().next().value).toEqual({
+				type: FETCH_LATEST_RATES_SUCCESS,
+				payload: {
+					success: true,
+					timestamp: 1578884105,
+					base: 'EUR',
+					date: '2020-01-13',
+					rates: {
+						DKK: 7.472523,
+						INR: 78.625903,
+						USD: 1.11235,
+						THB: 33.547923,
+						LKR: 201.214097
+					}
+				}
+			});
+		});
 	});
 });
