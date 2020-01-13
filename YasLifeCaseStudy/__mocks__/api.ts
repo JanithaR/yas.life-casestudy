e8c9c832a8657e3a056f6f5572e74f61fb6ddd68
Janitha Jeewantha Ranawansa <JanithaR@99x.lk>
2020-01-13 11:41:21 +0530

    [jest] 'api' mock

---
+++ YasLifeCaseStudy/__mocks__/api.ts
@@ -0,0 +1,17 @@
+export const makeRequest = () => {
+	console.log('MOCKING MAKE REQUEST');
+
+	return Promise.resolve({
+		success: true,
+		timestamp: 1578884105,
+		base: 'EUR',
+		date: '2020-01-13',
+		rates: {
+			DKK: 7.472523,
+			INR: 78.625903,
+			USD: 1.11235,
+			THB: 33.547923,
+			LKR: 201.214097
+		}
+	});
+};
