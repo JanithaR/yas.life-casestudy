23dcae40a43126027dd558ca7779009c7a767d9b
Janitha Ranawansa <janitha@Janithas-MBP.Home>
2020-07-24 17:28:06 +0530

    Set Prettier bracketSpacing to true

--- YasLifeCaseStudy_0_63_2/.prettierrc.js
+++ YasLifeCaseStudy_0_63_2/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 4,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
 };
