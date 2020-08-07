cce96d747a4a35017f4cda36712a329e7455befc
Janitha Ranawansa <janitha@Janithas-MBP.Home>
2020-08-07 16:00:09 +0530

    Use react-native-dotenv instead of babel-plugin-dotenv-import

--- YasLifeCaseStudy_0_63_2/babel.config.js
+++ YasLifeCaseStudy_0_63_2/babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
         [
-            'dotenv-import',
+            'module:react-native-dotenv',
             {
                 moduleName: '@env',
                 path: '.env',
